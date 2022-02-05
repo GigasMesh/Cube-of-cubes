ee4aff197b79dc93fdb9085f61d3c1a10db9e39e
Giovanne Souza <40374830+GigasMesh@users.noreply.github.com>
2022-02-04 22:35:43 -0300

    feat: final adjustments

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPa
 // Models
 let composer;
 let ssrPass, selects = [];
-let mixer_scene, mixer_sun_moon, mixer_dolphin;
+let mixer_animals, mixer_sun_moon, mixer_dolphin, mixer_bee;
 const params = {
     exposure: 0.7,
     bloomStrength: 0.159,
@@ -22,11 +22,9 @@ const params = {
     bloomRadius: 0.5
 };
 const loader = new GLTFLoader();
-loader.load('models/mine_scene7/scene.gltf', function (gltf) {
-    selects.push(gltf.scene.children[18].children[0])
-
+loader.load('models/mine_scene/scene.gltf', function (gltf) {
+    selects.push(gltf.scene.children[11].children[0])
     gltf.scene.traverse( function ( object ) {
-
         if ( object.isMesh ) {
             object.frustumCulled = false
             object.castShadow = true
@@ -60,14 +58,6 @@ loader.load('models/mine_scene7/scene.gltf', function (gltf) {
         }
     })
     scene.add(gltf.scene)
-
-    /*const model = gltf.scene
-    mixer_scene = new THREE.AnimationMixer(model)
-    const clips = gltf.animations
-    clips.forEach(function(clip){
-        const action = mixer_scene.clipAction(clip)
-        action.play()
-    })*/
 })
 
 loader.load('models/sun_moon/sun_moon.glb', function (gltf) {
@@ -79,10 +69,11 @@ loader.load('models/sun_moon/sun_moon.glb', function (gltf) {
     })
     scene.add(gltf.scene)
     const model = gltf.scene
-    mixer_dolphin = new THREE.AnimationMixer(model)
+    mixer_sun_moon = new THREE.AnimationMixer(model)
+    mixer_sun_moon.timeScale = 0.3
     const clips = gltf.animations
     clips.forEach(function(clip){
-        const action = mixer_dolphin.clipAction(clip)
+        const action = mixer_sun_moon.clipAction(clip)
         action.play()
     })
 })
@@ -96,33 +87,52 @@ loader.load('models/dolphin/dolphin.glb', function (gltf) {
     })
     scene.add(gltf.scene)
     const model = gltf.scene
-    mixer_sun_moon = new THREE.AnimationMixer(model)
+    mixer_dolphin = new THREE.AnimationMixer(model)
     const clips = gltf.animations
     clips.forEach(function(clip){
-        const action = mixer_sun_moon.clipAction(clip)
+        const action = mixer_dolphin.clipAction(clip)
+        action.play()
+    })
+})
+
+loader.load('models/animals/bee.glb', function (gltf) {
+    gltf.scene.traverse( function ( object ) {
+        if (object.isMesh){
+            object.frustumCulled = false
+            object.castShadow = true
+            object.material.depthWrite = true
+        }
+    })
+    scene.add(gltf.scene)
+    const model = gltf.scene
+    mixer_bee = new THREE.AnimationMixer(model)
+    const clips = gltf.animations
+    clips.forEach(function(clip){
+        const action = mixer_bee.clipAction(clip)
+        action.play()
+    })
+})
+
+loader.load('models/animals/others.glb', function (gltf) {
+    gltf.scene.traverse( function ( object ) {
+        if (object.isMesh){
+            object.frustumCulled = false
+            object.castShadow = true
+            object.material.depthWrite = true
+        }
+    })
+    scene.add(gltf.scene)
+    const model = gltf.scene
+    mixer_animals = new THREE.AnimationMixer(model)
+    const clips = gltf.animations
+    clips.forEach(function(clip){
+        const action = mixer_animals.clipAction(clip)
         action.play()
     })
 })
 
 // Debug
 const gui = new dat.GUI()
-
-gui.add( params, 'exposure', 0.1, 2 ).onChange( function ( value ) {
-    renderer.toneMappingExposure = Math.pow( value, 4.0 );
-} );
-
-gui.add( params, 'bloomThreshold', 0.0, 1.0 ).onChange( function ( value ) {
-    bloomPass.threshold = Number( value );
-} );
-
-gui.add( params, 'bloomStrength', 0.0, 3.0 ).onChange( function ( value ) {
-    bloomPass.strength = Number( value );
-} );
-
-gui.add( params, 'bloomRadius', 0.0, 1.0 ).step( 0.01 ).onChange( function ( value ) {
-    bloomPass.radius = Number( value );
-} );
-
 
 
 // Canvas
@@ -140,8 +150,6 @@ directionalLight.position.set(4,35,-4)
 directionalLight.castShadow = true
 directionalLight.shadow.bias = -0.0004
 scene.add( directionalLight );
-const directionalLightHelper = new THREE.PointLightHelper(directionalLight, 1)
-scene.add(directionalLightHelper)
 
 // GUI
 const light1 = gui.addFolder('Light 1')
@@ -149,6 +157,21 @@ light1.add(directionalLight.position, 'x').min(-100).max(100).step(1)
 light1.add(directionalLight.position, 'y').min(-100).max(100).step(1)
 light1.add(directionalLight.position, 'z').min(-100).max(100).step(1)
 light1.add(directionalLight, 'intensity').min(1).max(10).step(1)
+
+const bloom = gui.addFolder('Bloom')
+bloom.add( params, 'exposure', 0.1, 2 ).onChange( function ( value ) {
+    renderer.toneMappingExposure = Math.pow( value, 4.0 );
+} );
+bloom.add( params, 'bloomThreshold', 0.0, 1.0 ).onChange( function ( value ) {
+    bloomPass.threshold = Number( value );
+} );
+bloom.add( params, 'bloomStrength', 0.0, 3.0 ).onChange( function ( value ) {
+    bloomPass.strength = Number( value );
+} );
+bloom.add( params, 'bloomRadius', 0.0, 1.0 ).step( 0.01 ).onChange( function ( value ) {
+    bloomPass.radius = Number( value );
+} );
+
 
 /**
  * Sizes
@@ -178,9 +201,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 550)
-camera.position.x = 0
-camera.position.y = 50
-camera.position.z = 2
+camera.position.x = -18.253430065838124
+camera.position.y = 26.41143077011985
+camera.position.z = 38.510225079013196
 scene.add(camera)
 
 // Controls
@@ -220,7 +243,7 @@ ssrPass = new SSRPass( {
     selects: selects
 } );
 composer.addPass( renderScene );
-//composer.addPass( ssrPass );
+composer.addPass( ssrPass );
 composer.addPass( bloomPass );
 
 composer.addPass( new ShaderPass( GammaCorrectionShader ) );
@@ -235,22 +258,23 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elaspsedTime = clock.elapsedTime
-
+    const time = clock.getDelta()
     // Update Orbital Controls
     controls.update()
 
     // Render
-    if (mixer_scene){
-        mixer_scene.update(clock.getDelta())
+    if (mixer_dolphin){
+        mixer_dolphin.update(time)
+    }
+    if (mixer_bee){
+        mixer_bee.update(time)
+    }
+    if (mixer_animals){
+        mixer_animals.update(time)
     }
     if (mixer_sun_moon){
-        mixer_sun_moon.update(clock.getDelta())
+        mixer_sun_moon.update(time)
     }
-
-    /*
-    if (elaspsedTime>5){
-        renderer.setClearColor( 0xffffff, 1);
-    }*/
 
     composer.render();
 
